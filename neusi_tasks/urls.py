e69e8d2a4b5920d721b8d09ac8a0f130bb55f9a1
JCARDONA1650 <jorge80831@gmail.com>
2025-10-31 08:59:17 -0500

    docs(api): add /api/schema and /api/docs routes in root urls

--- neusi_tasks/urls.py
+++ neusi_tasks/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -8,5 +8,9 @@ urlpatterns = [
     path('api/backlog/auth/', include('backlog.api_urls')),
 
     # DRF (router)
-    path('api/backlog/', include('backlog.api_router')),  # 👈 aquí
+    path('api/backlog/', include('backlog.api_router')),  
+
+    # 👇 rutas de OpenAPI/Swagger en el raíz del proyecto
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
